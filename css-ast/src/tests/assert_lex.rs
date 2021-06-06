b71a2870aadea6ed01c462da0bdca37abb564514
Lukas Bombach <lukas.bombach@googlemail.com>
2021-06-06 22:58:52 +0200

    css-ast & ts-llvm-ir

---
+++ css-ast/src/tests/assert_lex.rs
@@ -0,0 +1,24 @@
+use logos::source::Source;
+use logos::Logos;
+
+use std::fmt;
+use std::ops::Range;
+
+pub fn assert_lex<'a, Token>(
+    source: &'a Token::Source,
+    tokens: &[(Token, &'a <Token::Source as Source>::Slice, Range<usize>)],
+) where
+    Token: Logos<'a> + fmt::Debug + PartialEq,
+    Token::Extras: Default,
+{
+    let mut lex = Token::lexer(source);
+
+    for tuple in tokens {
+        assert_eq!(
+            &(lex.next().expect("Unexpected end"), lex.slice(), lex.span()),
+            tuple
+        );
+    }
+
+    assert_eq!(lex.next(), None);
+}
