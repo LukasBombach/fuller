7b0be1e7f1033b3f4b216070f2102d57961b7019
Lukas Bombach <lukas.bombach@googlemail.com>
2021-06-09 23:27:05 +0200

    implemented a CSS Selector Lexer

--- css_lexer/src/lib.rs
+++ css_lexer/src/lib.rs
@@ -2,71 +2,87 @@ use logos::Logos;
 
 pub use logos::Lexer;
 
-pub enum Selector {
-    ClassName(String),
-}
 
-fn class_name(lex: &mut Lexer<Token>) -> Option<String>{
-    match lex.slice().strip_prefix(".") {
-        Some(name) => Some(name.to_string()),
-        None => None
-    }
-}
-
-/* #[derive(Logos, Debug, PartialEq, Clone)]
-pub enum CssSelector {
+#[derive(Logos, Debug, PartialEq, Clone, Copy)]
+pub enum Selector<'a> {
     #[regex(r"[ \t\n\f]+", logos::skip)]
     #[error]
     Error,
 
-    #[regex("[a-zA-Z0-9_-]+")]
-    Ident,
-} */
+    #[regex("[a-zA-Z0-9_-]+", |lex| lex.slice())]
+    Tag(&'a str),
 
-#[derive(Logos, Debug, PartialEq, Clone)]
-pub enum Token {
-    #[regex(r"[ \t\n\f]+", logos::skip)]
-    #[error]
-    Error,
+    #[regex("\\.[a-zA-Z0-9_-]+", |lex| lex.slice().strip_prefix("."))]
+    Class(&'a str),
 
+    #[regex("#[a-zA-Z0-9_-]+", |lex| lex.slice().strip_prefix("#"))]
+    Id(&'a str),
 
-    #[regex("\\.[-a-zA-Z_][a-zA-Z0-9_-]*", class_name)]
-    ClassName(String),
-
-    #[regex("em|ex|ch|rem|vw|vh|vmin|vmax")]
-    RelativeLength,
-
-    #[regex("cm|mm|Q|in|pc|pt|px")]
-    AbsoluteLength,
-
-    #[regex("[+-]?[0-9]*[.]?[0-9]+(?:[eE][+-]?[0-9]+)?", priority = 2)]
-    Number,
-
-    #[regex("[-a-zA-Z_][a-zA-Z0-9_-]*")]
-    Ident,
+    #[token(",")]
+    Comma,
 
     #[token("{")]
     CurlyBracketOpen,
-
-    #[token("}")]
-    CurlyBracketClose,
-
-    #[token("::")]
-    DoubleColon,
-
-    #[token(":")]
-    Colon,
-
-    #[token(";")]
-    SemiColon,
-
-    #[token(".")]
-    Dot,
-
-    #[token("#")]
-    Hash,
 }
 
-pub fn get_lexer<'a>(source: &'a str) -> logos::Lexer<'a, Token>  {
-    Token::lexer(source)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tag_selector() {
+        let tokens: Vec<_> = Selector::lexer("div").collect();
+        assert_eq!(tokens, &[Selector::Tag("div")]);
+    }
+
+    #[test]
+    fn test_class_selector() {
+        let tokens: Vec<_> = Selector::lexer(".my_class").collect();
+        assert_eq!(tokens, &[Selector::Class("my_class")]);
+
+    }
+
+    #[test]
+    fn test_id_selector() {
+        let tokens: Vec<_> = Selector::lexer("#my_id").collect();
+        assert_eq!(tokens, &[Selector::Id("my_id")]);
+
+    }
+
+    #[test]
+    fn test_child_selectors() {
+        let tokens: Vec<_> = Selector::lexer("div .my_class #my_id").collect();
+        assert_eq!(tokens, &[
+            Selector::Tag("div"),
+            Selector::Class("my_class"),
+            Selector::Id("my_id"),
+        ]);
+    }
+
+    #[test]
+    fn test_multiple_selectors() {
+        let tokens: Vec<_> = Selector::lexer("div, .my_class, #my_id").collect();
+        assert_eq!(tokens, &[
+            Selector::Tag("div"),
+            Selector::Comma,
+            Selector::Class("my_class"),
+            Selector::Comma,
+            Selector::Id("my_id"),
+        ]);
+    }
+
+    #[test]
+    fn test_comma() {
+        let tokens: Vec<_> = Selector::lexer(",").collect();
+        assert_eq!(tokens, &[Selector::Comma]);
+
+    }
+
+    #[test]
+    fn test_curly_bracket_open() {
+        let tokens: Vec<_> = Selector::lexer("{").collect();
+        assert_eq!(tokens, &[Selector::CurlyBracketOpen]);
+
+    }
+
 }
