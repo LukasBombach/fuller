e682992ad4a6fb242ad9ceb27f61de43e3aa4b79
Lukas Bombach <lukas.bombach@googlemail.com>
2021-10-25 13:50:41 +0200

    wip

--- crates/estree-parser/src/main.rs
+++ crates/estree-parser/src/main.rs
@@ -1,16 +1,41 @@
+extern crate ucd;
 use std::str::Chars;
+use std::string::String;
+use ucd::Codepoint;
 
 #[derive(Debug, PartialEq)]
 pub enum Thing {
-    PossiblyConst,
+    Const,
+    Identifier,
     Unknown(char),
     Nothing,
+}
+
+fn matches_next(code: &mut Chars, fragment: &'static str) -> bool {
+    for c in fragment.chars() {
+        if Some(c) != code.next() {
+            return false;
+        }
+    }
+    true
 }
 
 #[allow(clippy::needless_return)]
 fn detect_the_thing(code: &mut Chars) -> Thing {
     return match code.next() {
-        Some('c') => Thing::PossiblyConst,
+        Some('c') => {
+            if matches_next(code, "onst") {
+                if let Some(c) = code.next() {
+                    if c.is_id_continue() {
+                        return Thing::Identifier;
+                    } else {
+                        /* only true if first character of expression? */
+                        return Thing::Const;
+                    }
+                }
+            }
+            Thing::Unknown('x')
+        }
         Some(c) => Thing::Unknown(c),
         None => Thing::Nothing,
     };
@@ -20,5 +45,8 @@ fn main() {
     let mut code = "const x = 1;".chars();
     let what_we_have = detect_the_thing(&mut code);
 
-    println!("\n\nWe got: {:?}\n\n", what_we_have);
+    println!("\n\n");
+    println!("Got   {:?}", what_we_have);
+    println!("Rest  {}", code.collect::<String>());
+    println!("\n\n");
 }
