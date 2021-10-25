0e79bf997a5c6fe3936a15b1b4a42cbd44eae353
Lukas Bombach <lukas.bombach@googlemail.com>
2021-10-25 12:50:50 +0200

    wip

--- crates/estree-parser/src/main.rs
+++ crates/estree-parser/src/main.rs
@@ -1,5 +1,24 @@
-mod program;
+use std::str::Chars;
+
+#[derive(Debug, PartialEq)]
+pub enum Thing {
+    PossiblyConst,
+    Unknown(char),
+    Nothing,
+}
+
+#[allow(clippy::needless_return)]
+fn detect_the_thing(code: &mut Chars) -> Thing {
+    return match code.next() {
+        Some('c') => Thing::PossiblyConst,
+        Some(c) => Thing::Unknown(c),
+        None => Thing::Nothing,
+    };
+}
 
 fn main() {
-    program::get_ast(";".chars());
+    let mut code = "const x = 1;".chars();
+    let what_we_have = detect_the_thing(&mut code);
+
+    println!("\n\nWe got: {:?}\n\n", what_we_have);
 }
