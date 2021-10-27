a867daf5ddac01c1aadf7318bb265bf2eadc13bb
Lukas Bombach <lukas.bombach@googlemail.com>
2021-10-27 22:29:22 +0200

    wrote identifier matcher

--- crates/estree-parser/src/scanner.rs
+++ crates/estree-parser/src/scanner.rs
@@ -7,6 +7,7 @@ use ucd::Codepoint;
 // todo avoid heap allocation with String for performance
 #[derive(Debug, PartialEq)]
 pub enum Token {
+  Identifier(String),
   Literal(String),
   VariableDeclaration(String),
   LineBreak,
@@ -35,9 +36,9 @@ impl<'a> Iterator for Scanner<'a> {
   fn next(&mut self) -> Option<Self::Item> {
     loop {
       match self.input.next() {
-        Some(c) if self.is_whitespace(c) => continue,
-        Some(c) if self.is_id_start(c) => return self.identifier(c),
-        Some(c) if self.is_quote(c) => return self.literal(c),
+        Some(c) if self.is_whitespace(&c) => continue,
+        Some(c) if self.is_id_start(&c) => return self.identifier(&c),
+        Some(c) if self.is_quote(&c) => return self.literal(&c),
         Some('\n') => return Some(Token::LineBreak),
         Some('\r') => continue,
         None => return Some(Token::Eos),
@@ -50,7 +51,7 @@ impl<'a> Iterator for Scanner<'a> {
  * Tokens
  */
 impl<'a> Scanner<'a> {
-  fn literal(&mut self, quote_symbol: char) -> Option<Token> {
+  fn literal(&mut self, quote_symbol: &char) -> Option<Token> {
     let mut value = String::new();
     loop {
       match self.input.next() {
@@ -61,7 +62,7 @@ impl<'a> Scanner<'a> {
             None => break,
           }
         }
-        Some(c) if c == quote_symbol => break,
+        Some(c) if c == *quote_symbol => break,
         Some(c) => value.push(c),
         None => break,
       }
@@ -69,21 +70,43 @@ impl<'a> Scanner<'a> {
     Some(Token::Literal(value))
   }
 
-  fn identifier(&mut self, first_char: char) -> Option<Token> {}
+  fn identifier(&mut self, first_char: &char) -> Option<Token> {
+    let mut value = first_char.to_string();
+
+    let continued_value = self
+      .input
+      .by_ref()
+      .take_while(|c| match c {
+        '$' | '_' | 'A'..='Z' | 'a'..='z' | '0'..='9' | '\u{005C}' => true,
+        _ => c.is_id_continue(),
+      })
+      .collect::<String>();
+
+    value.push_str(&continued_value);
+
+    return Some(Token::Identifier(value));
+  }
 }
 
 /**
  * Matchers
  */
 impl<'a> Scanner<'a> {
-  fn is_id_start(&self, c: char) -> bool {
+  fn is_id_start(&self, c: &char) -> bool {
     match c {
       '$' | '_' | 'A'..='Z' | 'a'..='z' => true,
       _ => c.is_id_start(),
     }
   }
 
-  fn is_whitespace(&self, c: char) -> bool {
+  fn is_id_continue(&self, c: &char) -> bool {
+    match c {
+      '$' | '_' | 'A'..='Z' | 'a'..='z' | '0'..='9' | '\u{005C}' => true,
+      _ => c.is_id_continue(),
+    }
+  }
+
+  fn is_whitespace(&self, c: &char) -> bool {
     match c {
       '\u{0009}' | '\u{000B}' | '\u{000C}' | '\u{0020}' | '\u{00A0}' | '\u{FEFF}' | '\u{1680}'
       | '\u{2000}' | '\u{2001}' | '\u{2002}' | '\u{2003}' | '\u{2004}' | '\u{2005}'
@@ -93,7 +116,7 @@ impl<'a> Scanner<'a> {
     }
   }
 
-  fn is_quote(&self, c: char) -> bool {
+  fn is_quote(&self, c: &char) -> bool {
     match c {
       '"' | '\'' => true,
       _ => false,
