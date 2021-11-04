fce1039b26e544b06bffc6a456f8b955b897b249
Lukas Bombach <lukas.bombach@googlemail.com>
2021-11-04 11:11:15 +0100

    MOVED TO ALT_SCANNERS

--- crates/estree-parser/src/main.rs
+++ crates/estree-parser/src/main.rs
@@ -1,13 +1,7 @@
-#![feature(iter_advance_by)]
-
 pub mod scanner;
-pub mod scanner_chars;
-pub mod scanner_str_slice;
 pub mod token;
-pub mod token_chars;
-pub mod token_string;
 
-use scanner_str_slice::Scanner;
+use scanner::Scanner;
 
 fn main() {
   let scanner = Scanner::new("   const myvar = 'my value';");
--- crates/estree-parser/src/scanner.rs
+++ crates/estree-parser/src/scanner.rs
@@ -1,24 +1,67 @@
 use crate::token::Token;
+use std::iter::Enumerate;
+use std::str::Chars;
+
+pub struct Source<'src> {
+  input: &'src str,
+  chars: Enumerate<Chars<'src>>,
+  loc: Location,
+}
+
+impl<'src> Source<'src> {
+  pub fn new(input: &'src str) -> Self {
+    println!("\nInput `{}`\n", input);
+    Self {
+      input,
+      chars: input.chars().enumerate(),
+      loc: Location { line: 0, column: 0 },
+    }
+  }
+  pub fn slice(&self, start: usize, end: usize) -> &'src str {
+    &self.input[start..end]
+  }
+}
+
+impl<'src> Iterator for Source<'src> {
+  type Item = (usize, char);
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if let Some(n) = self.chars.next() {
+      match n {
+        (_, '\r') => {}
+        (_, '\n') => self.loc.newline(),
+        (_, _) => self.loc.char(),
+      }
+      return Some(n);
+    }
+    None
+  }
+}
 
 pub struct Location {
   pub line: usize,
   pub column: usize,
 }
 
+impl Location {
+  pub fn char(&mut self) {
+    self.column += 1;
+  }
+
+  pub fn newline(&mut self) {
+    self.column = 0;
+    self.line += 1;
+  }
+}
+
 pub struct Scanner<'src> {
-  source: &'src str,
-  loc: Location,
-  len: usize,
-  pos: usize,
+  source: Source<'src>,
 }
 
 impl<'src> Scanner<'src> {
-  pub fn new(source: &'src str) -> Self {
+  pub fn new(input: &'src str) -> Self {
     Self {
-      source,
-      loc: Location { line: 0, column: 0 },
-      len: source.len(),
-      pos: 0,
+      source: Source::new(input),
     }
   }
 }
@@ -27,87 +70,24 @@ impl<'src> Iterator for Scanner<'src> {
   type Item = Token<'src>;
 
   fn next(&mut self) -> Option<Self::Item> {
-    if self.pos == self.len {
-      return None;
+    while let Some(n) = self.source.next() {
+      match n {
+        (_, ' ') => {}
+        (i, c) if matches!(c, 'a'..='z' | 'A'..='Z' | '_' | '$') => return self.identifier(i),
+        (i, c) => println!("{} {}", i, c),
+      }
     }
-
-    // todo this is akward
-    self.skip_whitespace();
-
-    // todo use pointers, not values
-    // todo calling as_bytes eacht time is BAAD
-    let next_token = match self.source.as_bytes()[self.pos] as char {
-      '=' => self.token(Token::Eq, 1),
-      ';' => self.token(Token::Semicolon, 1),
-      '\n' => self.newline(1),
-      '\r' => self.newline(1),
-      c if matches!(c, 'a'..='z' | 'A'..='Z' | '_' | '$') => return self.identifier(),
-      // c if matches!(c, '"' | '\'') => return self.literal(c),
-      // c if matches!(c, '0'..='9') => return self.number(c),
-      // todo c matches unicode ID_Start
-      c => self.token(Token::Unknown(c), 1),
-    };
-
-    next_token
+    None
   }
 }
 
 impl<'src> Scanner<'src> {
-  // todo this seems like a highly ineffective implementation
   #[inline]
-  fn find_offset<P>(&self, predicate: P) -> usize
-  where
-    Self: Sized,
-    P: Fn(char) -> bool,
-  {
-    let source_from_pos = &self.source[self.pos..];
-    match source_from_pos.find(predicate) {
-      Some(offset) => offset,
-      None => source_from_pos.len(),
+  fn identifier(&mut self, start: usize) -> Option<Token<'src>> {
+    if let Some((end, _)) = self.source.by_ref().take_while(|(_, c)| *c != ' ').last() {
+      let value = self.source.slice(start, end + 1);
+      return Some(Token::Identifier(value));
     }
-  }
-
-  // todo this is even worse than find_offset
-  #[inline]
-  fn find_position<P>(&self, predicate: P) -> usize
-  where
-    Self: Sized,
-    P: Fn(char) -> bool,
-  {
-    let offset = self.find_offset(predicate);
-    self.pos + offset
-  }
-}
-
-impl<'src> Scanner<'src> {
-  #[inline]
-  fn skip_whitespace(&mut self) {
-    let idx = self.find_offset(|c| c != ' ');
-    self.pos += idx;
-    self.loc.column += idx;
-  }
-}
-
-impl<'src> Scanner<'src> {
-  #[inline]
-  fn identifier(&mut self) -> Option<Token<'src>> {
-    let end = self.find_position(|c| c == ' ');
-    let val = &self.source[self.pos..end];
-    return self.token(Token::Identifier(val), end - self.pos);
-  }
-
-  #[inline]
-  fn token(&mut self, token: Token<'src>, len: usize) -> Option<Token<'src>> {
-    self.pos += len;
-    self.loc.column += len;
-    Some(token)
-  }
-
-  #[inline]
-  fn newline(&mut self, len: usize) -> Option<Token<'src>> {
-    self.pos += len;
-    self.loc.column = 0;
-    self.loc.line += 1;
-    Some(Token::Newline)
+    None
   }
 }
