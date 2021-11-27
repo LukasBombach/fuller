646eff3df56b5e64253bbe1553086d8dcf02646d
Lukas Bombach <lukas.bombach@googlemail.com>
2021-11-27 12:47:04 +0100

    wip mofo

--- crates/parser/src/main.rs
+++ crates/parser/src/main.rs
@@ -1,9 +1,20 @@
 use lexer::tokenize;
+use lexer::TokenKind;
 
 fn main() {
-    let tokens = tokenize("const x = 'foobar';");
+    let mut tokens = tokenize("const x = 'foo';\nconst y = 'bar';");
+    // let mut line: usize = 1;
+    // let mut col: usize = 1;
 
-    tokens
-        .into_iter()
-        .for_each(|token| println!("{:#?}", token));
+    while let Some(token) = tokens.next() {
+        println!("{:#?}", token.kind);
+
+        // match token.kind {
+        // TokenKind::Ident => {
+
+        // }
+        // }
+
+        // col = col + token.len;
+    }
 }
