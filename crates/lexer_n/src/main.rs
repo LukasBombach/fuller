4ddbf42ad7def5e01964c3c13893d7a3d7ef0044
Lukas Bombach <lukas.bombach@googlemail.com>
2022-03-27 23:02:10 +0200

    wip

--- crates/lexer_n/src/main.rs
+++ crates/lexer_n/src/main.rs
@@ -1,13 +1,10 @@
-use match_keyword_derive::match_keyword;
-
-match_keyword!();
+use match_keyword::match_keyword;
 
 fn main() {
     let input = "const";
-    match_keyword!(SELECT * FROM posts WHERE id=1);
-    /* (input, {
+    match_keyword!(input {
         "const" => Const,
         "let" => Let,
         "var" => Var,
-    }); */
+    });
 }
--- crates/match_keyword/src/lib.rs
+++ crates/match_keyword/src/lib.rs
@@ -1,3 +1,18 @@
-pub trait MatchKeyword {
-    fn match_keyword();
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+use syn::ExprMatch;
+
+#[proc_macro]
+pub fn match_keyword(input: TokenStream) -> TokenStream {
+    let syntax: ExprMatch = syn::parse(input).unwrap();
+
+    println!("{}", quote!(#syntax));
+
+    let gen = quote! {
+        12
+    };
+    gen.into()
 }
