12f854308bb4d17867431a459cb11de526f6e9fd
Lukas Bombach <lukas.bombach@googlemail.com>
2022-03-28 09:56:01 +0200

    verbose first, clever next

--- crates/ecma_lexer/src/main.rs
+++ crates/ecma_lexer/src/main.rs
@@ -1,27 +1,44 @@
 use TokenKind::*;
 
+#[derive(Debug)]
 enum TokenKind {
+    CONST,
+    IDENT,
     EOF,
+    UNEXPECTED,
 }
 
 
-
 fn main() {
-    let input = "const foo = true;";
-    let mut tokens =  Vec::new<TokenKind>();
-
+    let input = "const conrad";
+    let mut tokens =  Vec::new();
     let mut chars = input.chars();
 
     loop {
-        let c = chars.next().unwrap_or_else(|| {
-            tokens.push(EOF);
-            break   
-            
-             });
-
-
-        match c {
-            'c' => {}
+        match chars.next().unwrap_or_default() {
+            'c' => {
+                match chars.next().unwrap_or_default() {
+                    'o' => match chars.next().unwrap_or_default() {
+                        'n' => match chars.next().unwrap_or_default() {
+                            's' => match chars.next().unwrap_or_default() {
+                                't' => tokens.push(CONST),
+                                _ => tokens.push(IDENT),
+                            },
+                            _ => tokens.push(IDENT),
+                        },
+                        _ => tokens.push(IDENT),
+                    },
+                    _ => tokens.push(IDENT),
+                }
+            }
+            '\x00' => {
+                tokens.push(EOF);
+                break;
+            }
+            _ => tokens.push(UNEXPECTED)
         }
+
     }
+
+    println!("{:#?}", tokens);
 }
