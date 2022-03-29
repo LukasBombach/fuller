9d555a1dee0173256e970a45e4561833ce6ec023
Lukas Bombach <lukas.bombach@googlemail.com>
2022-03-29 09:30:34 +0200

    gml

--- crates/ecma_lexer/src/main.rs
+++ crates/ecma_lexer/src/main.rs
@@ -10,6 +10,55 @@ enum Keyword {
     Semi,
 }
 
+/* break
+
+case
+catch
+class
+const
+continue
+
+debugger
+default
+delete
+do
+
+else
+export
+extends
+
+false
+finally
+for
+function
+
+if
+import
+in
+instanceof
+
+new
+null
+
+return
+
+super
+switch
+
+this
+throw
+true
+try
+typeof
+
+var
+void
+
+while
+with
+
+yield */
+
 fn main() {
     let input = "const conrad = true;";
     match_keyword!(match input {
